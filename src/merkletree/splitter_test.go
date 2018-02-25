fbb501b004711ca04df0183d8e46d60e1aee5e7d
burton <burton@spinn3r.com>
2018-02-25 12:10:50 -0800

    ok.. some basic test functions now

--- src/merkletree/splitter_test.go
+++ src/merkletree/splitter_test.go
@@ -5,23 +5,41 @@ import (
 	"fmt"
 )
 
-func assertEqual(t *testing.T, a interface{}, b interface{}, message string) {
-	if a == b {
+func assertEqual(t *testing.T, obj0 interface{}, obj1 interface{}, message string) {
+
+	// TODO: the limitation here for now is that we convert each object to
+	// a string which is a bit of a hack for now. We should use a library that
+	// has support for a better assertEquals function.
+
+	str0 := fmt.Sprintf("%+v", obj0)
+	str1 := fmt.Sprintf("%+v", obj1)
+
+	if str0 == str1 {
 		return
 	}
+
 	if len(message) == 0 {
-		message = fmt.Sprintf("%v != %v", a, b)
+		message = fmt.Sprintf("%s != %s", str0, str1)
 	}
+
 	t.Fatal(message)
+
 }
 
-func TestComputeSlabReferences(t *testing.T) {
+func TestComputeSlabReferencesWithExactBounds(t *testing.T) {
+	
+	actual := ComputeSlabReferences(10, 10)
+	expected := []SlabReference{{0,10}}
 
-	fmt.Printf("Test worked!\n")
-	t.Log("worked fine")
+	assertEqual(t, actual, expected, "")
 
-	references := ComputeSlabReferences(10, 2)
+}
 
-	assertEqual(t, references, "", "")
+func TestComputeSlabReferencesWithTruncatedbounds(t *testing.T) {
+
+	actual := ComputeSlabReferences(12, 10)
+	expected := []SlabReference{{0,10},{10,2}}
+
+	assertEqual(t, actual, expected, "")
 
 }
